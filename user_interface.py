0ca03fc210eb789d646504cfbc7c0d21d591e5aa
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-04-04 19:55:28 -0400

    The previous commit did not include the user interface and user link files.

--- user_interface.py
+++ user_interface.py
@@ -7,6 +7,10 @@ from kivy.uix.textinput import TextInput
 import user_link
 variables_for_user_link = []
 
+import database
+movie_database = ["Wait"]
+filter_services_list = []
+
 data_structure_movies_list = []
 seen_movies_list = []
 
@@ -16,6 +20,10 @@ class MovieMatcher(App):
 
         global variables_for_user_link
         variables_for_user_link = []
+        global movie_database
+        movie_database = ["Wait"]
+        global filter_services_list
+        filter_services_list = []
 
         # Let Kivy handle spacing of widgets
         # Kivy will put everything in one column
@@ -42,6 +50,21 @@ class MovieMatcher(App):
         self.input_IP = TextInput(multiline=False)
         self.connect_button = Button(text="connect")
         self.connect_button.bind(on_press=self.press_connect_button)
+
+        # We might restrict filtering to the host
+        # We can use buttons to select what to filter
+        # For now we will just have buttons for streaming services
+        self.netflix_button = Button(text="Netflix")
+        self.netflix_button.bind(on_press=self.press_netflix_button)
+        self.disney_plus_button = Button(text="Disney Plus")
+        self.disney_plus_button.bind(on_press=self.press_disney_plus_button)
+        self.amazon_prime_button = Button(text="Amazon Prime")
+        self.amazon_prime_button.bind(on_press=self.press_amazon_prime_button)
+        self.hulu_button = Button(text="Hulu")
+        self.hulu_button.bind(on_press=self.press_hulu_button)
+        # When the host has selected the streaming services to be used, We'll need to create a pandas dataframe
+        self.next_button = Button(text="next")
+        self.next_button.bind(on_press=self.press_next_button)
 
         #######################################################################################################################################################
         # A label for testing purposes
@@ -83,7 +106,7 @@ class MovieMatcher(App):
         self.window.remove_widget(self.host_button)
         self.window.remove_widget(self.join_button)
         variables_for_user_link.append(True) # boolean value to stop the host's accepting thread
-        server, thread = user_link.start_hosting(variables_for_user_link)
+        server, thread = user_link.start_hosting(variables_for_user_link, movie_database)
         variables_for_user_link.append(server) # storing the socket that the host is using so that it can be shut down later
         variables_for_user_link.append(thread) # storing the host's accepting thread for proper termination later
         self.window.add_widget(self.IP_label)
@@ -107,14 +130,11 @@ class MovieMatcher(App):
         variables_for_user_link[7].join()
         self.window.remove_widget(self.IP_label)
         self.window.remove_widget(self.start_button)
-
-        #######################################################################################################################################################
-        # The testing label is used for testing purposes
-        # The code can be removed after testing is complete
-        self.window.add_widget(self.testing_label)
-        self.window.add_widget(self.host_shutdown_button)
-        variables_for_user_link[0] = self
-        #######################################################################################################################################################
+        self.window.add_widget(self.netflix_button)
+        self.window.add_widget(self.disney_plus_button)
+        self.window.add_widget(self.amazon_prime_button)
+        self.window.add_widget(self.hulu_button)
+        self.window.add_widget(self.next_button)
 
 
     # The non-host has entered the host's IP address into the text input and would like to connect to the host
@@ -127,6 +147,7 @@ class MovieMatcher(App):
         client, thread = user_link.client_join_server(self.input_IP.text, variables_for_user_link)
         variables_for_user_link.append(client) # storing the socket used by the client so that it can be shutdown later
         variables_for_user_link.append(thread) # storing the thread used by the client so that it can be properly terminated later
+        #movie_database.append(database.generate_database(["netflix", "disney_plus", "amazon_prime", "hulu"]))
         self.window.remove_widget(self.input_IP)
         self.window.remove_widget(self.connect_button)
         self.window.add_widget(self.client_movie_label)
@@ -137,6 +158,70 @@ class MovieMatcher(App):
         self.window.add_widget(self.client_downvote_button)
         self.window.add_widget(self.client_shutdown_button)
 
+
+    # Function to add or remove Netflix from filtering
+    def press_netflix_button(self, instance):
+        global filter_services_list
+        if (self.netflix_button.text == "Netflix"):
+            filter_services_list.append("netflix")
+            self.netflix_button.text = "Netflix added"
+        else:
+            filter_services_list.remove("netflix")
+            self.netflix_button.text = "Netflix"
+
+
+    # Function to add or remove Disney Plus from filtering
+    def press_disney_plus_button(self, instance):
+        global filter_services_list
+        if (self.disney_plus_button.text == "Disney Plus"):
+            filter_services_list.append("disney_plus")
+            self.disney_plus_button.text = "Disney Plus added"
+        else:
+            filter_services_list.remove("disney_plus")
+            self.disney_plus_button.text = "Disney Plus"
+
+
+    # Function to add or remove Amazon Prime from filtering
+    def press_amazon_prime_button(self, instance):
+        global filter_services_list
+        if (self.amazon_prime_button.text == "Amazon Prime"):
+            filter_services_list.append("amazon_prime")
+            self.amazon_prime_button.text = "Amazon Prime added"
+        else:
+            filter_services_list.remove("amazon_prime")
+            self.amazon_prime_button.text = "Amazon Prime"
+
+
+    # Function to add or remove Hulu from filtering
+    def press_hulu_button(self, instance):
+        global filter_services_list
+        if (self.hulu_button.text == "Hulu"):
+            filter_services_list.append("hulu")
+            self.hulu_button.text = "Hulu added"
+        else:
+            filter_services_list.remove("hulu")
+            self.hulu_button.text = "Hulu"
+
+
+    # Function to create the pandas dataframe when the host has selected the filters for the streaming services
+    def press_next_button(self, instance):
+        self.window.remove_widget(self.netflix_button)
+        self.window.remove_widget(self.disney_plus_button)
+        self.window.remove_widget(self.amazon_prime_button)
+        self.window.remove_widget(self.hulu_button)
+        self.window.remove_widget(self.next_button)
+        #######################################################################################################################################################
+        # The testing label is used for testing purposes
+        # The code can be removed after testing is complete
+        self.window.add_widget(self.testing_label)
+        self.window.add_widget(self.host_shutdown_button)
+        variables_for_user_link[0] = self
+        #######################################################################################################################################################
+        movie_database.append(database.generate_database(filter_services_list))
+        movie_database[0] = ""
+
+
+    # Function to allow the client to upvote a title
     def press_client_upvote_button(self, instance):
         if not variables_for_user_link[2]:
             return
@@ -149,6 +234,8 @@ class MovieMatcher(App):
             self.window.remove_widget(self.client_upvote_button)
             self.window.remove_widget(self.client_downvote_button)
 
+
+    # Function to allow the client to downvote a title
     def press_client_downvote_button(self, instance):
         if not variables_for_user_link[2]:
             return
@@ -160,6 +247,7 @@ class MovieMatcher(App):
         if variables_for_user_link[1] == "\x09shutdown":
             self.window.remove_widget(self.client_upvote_button)
             self.window.remove_widget(self.client_downvote_button)
+
 
     #######################################################################################################################################################
     # Function to be bound to temporary host shutdown button
